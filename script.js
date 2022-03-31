0704c89dff5da9ffa3d02f9571c57ea67dec74bd
Amit Singh <abcdamit44@gmail.com>
2022-03-31 11:19:49 +0530

    Add CSS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $("#selectItems").change(function () {
-    var options = parseInt($(this).children("option:selected").val());
+
+    let options = parseInt($(this).children("option:selected").val());
     $('.check').click(() => {
         let totalCost = 0;
         let totalBenefit = 0;
@@ -7,8 +8,12 @@ $("#selectItems").change(function () {
             totalCost += parseInt($('.check:checked').attr("data-cost"));
             totalBenefit += parseInt($('.check:checked').attr("data-benefit"));
         })
-        $('#totalOne').html(`$ ${totalCost + options}`)
+        console.log(totalCost + options);
+        $('#totalOne').html(`$ ${totalCost + options
+            }`)
         $('#totalTwo').html(`$ ${totalBenefit + options}`)
+
+
     })
 
 });
